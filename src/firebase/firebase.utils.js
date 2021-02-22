6c69102e6eb579f1c653e2e1c54d0f2fe973f8e4
SmdlDom <38593146+SmdlDom@users.noreply.github.com>
2021-02-21 20:03:39 -0500

    Wired data onto firestore and implemented a spinner for loading

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -46,6 +46,24 @@ export const addCollectionAndItems = async (collectionKey, objectsToAdd) => {
 	});
 
 	return await batch.commit()
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+	const transformedCollection = collections.docs.map(doc => {
+		const { title, items } = doc.data();
+
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items
+		}
+	});
+
+	return transformedCollection.reduce((accumulator, collection) => {
+		accumulator[collection.title.toLowerCase()] = collection;
+		return accumulator;
+	}, {});
 }
 
 if (!firebase.apps.length) {
